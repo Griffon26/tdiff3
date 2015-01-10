1d89bffb956d7d48b430e085702f130492d93d28
Maurice van der Pot <griffon26@kfk4ever.com>
2015-01-10 16:47:39 +0100

    Made path to ycurses relative

--- source/diff.d
+++ source/diff.d
@@ -19,6 +19,7 @@
  * with tdiff3; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+
 import std.container;
 import std.stdio;
 import std.typecons;
